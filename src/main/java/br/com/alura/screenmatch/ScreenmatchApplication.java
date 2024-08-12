9c21df7d40c7fef43d2ae4969d8de48364df33df
Robson Martins <robson.java@gmail.com>
2024-08-12 20:32:05 -0300

    .

--- src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.alura.screenmatch.model.DadosEpisodio;
 import br.com.alura.screenmatch.model.DadosSerie;
 import br.com.alura.screenmatch.service.ConsumoApi;
 import br.com.alura.screenmatch.service.ConverteDados;
@@ -21,11 +22,16 @@ public class ScreenmatchApplication implements CommandLineRunner {
 		var api = new ConsumoApi();
 		var json = api.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=6585022c");
 
-		System.out.println(json);
+//		System.out.println(json);
+//		System.out.println(json);
+		
 		ConverteDados conversor = new ConverteDados();
-		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
-		System.out.println(dados);
-		System.out.println(json);
+		
+		DadosSerie dadosSerie = conversor.obterDados(json, DadosSerie.class);
+		DadosEpisodio dadosEpisodio = conversor.obterDados(json, DadosEpisodio.class);
+				
+		System.out.println(dadosSerie);
+		System.out.println(dadosEpisodio);
 	}
 
 }
